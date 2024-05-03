9fba3bb1d61b23d318cab678cc5ea03ef04dddac
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-05-03 21:55:54 +0100

    new value waiting received from reques body and pass it to bookings.insertOne

---
+++ pages/api/bookings.ts
@@ -0,0 +1,30 @@
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Booking } from "../../types/booking";
+import clientPromise from "../../lib/mongodb";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const client = await clientPromise;
+    const db = client.db();
+    const bookings = db.collection<Booking>("bookings");
+
+    try {
+      const { activity, name, email, waiting } = JSON.parse(req.body);
+      await bookings.insertOne({
+        activity: ObjectId.createFromHexString(activity),
+        name,
+        email,
+        createdAt: new Date(),
+        waiting
+      });
+      res.status(201).json({ message: "Booking created" });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Error creating booking" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
