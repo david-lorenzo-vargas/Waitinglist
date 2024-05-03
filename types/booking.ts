729667729cf01b0cd6830cb318a6f8764a66087f
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-05-03 21:54:39 +0100

    new value added to bookings types waiting: boolean

---
+++ types/booking.ts
@@ -0,0 +1,10 @@
+import { ObjectId } from "bson";
+
+export interface Booking<ID = ObjectId, A = ObjectId> {
+  _id?: ID;
+  name: string;
+  email: string;
+  activity: A;
+  createdAt: Date;
+  waiting: boolean;
+}
